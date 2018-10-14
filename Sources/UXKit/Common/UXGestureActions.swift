d279e003249744a9130875b08f0808cbd8a75b83
Helge Heß <devteam@zeezide.de>
2018-10-14 16:47:05 +0200

    Add `onSecondaryTap`
    This sets up a right-click gesture recognizer on
    AppKit, and a long-tap one on UIKit.

--- Sources/UXKit/Common/UXGestureActions.swift
+++ Sources/UXKit/Common/UXGestureActions.swift
@@ -6,7 +6,11 @@
 //  Copyright © 2017 ZeeZide GmbH. All rights reserved.
 //
 
-import Foundation
+#if os(macOS)
+  import AppKit
+#else
+  import UIKit
+#endif
 
 public extension UXView {
   
@@ -26,6 +30,22 @@ public extension UXView {
     return on(gesture: UXTapGestureRecognizer(), target: target, action: action)
   }
   
+  /**
+   * On macOS this reacts to the secondary mouse button, and on iOS to the
+   * "long press".
+   */
+  @discardableResult
+  func onSecondaryTap(_ target: AnyObject, _ action: Selector) -> Self {
+    #if os(macOS)
+      let gr = NSClickGestureRecognizer()
+      gr.buttonMask = 0x2 // secondary button
+      return on(gesture: gr, target: target, action: action)
+    #else
+      let gr = UILongPressGestureRecognizer()
+      return on(gesture: gr, target: target, action: action)
+    #endif
+  }
+
   @discardableResult
   func onPinch(_ target: AnyObject, _ action: Selector) -> Self {
     return on(gesture: UXPinchGestureRecognizer(),
