381a8a0fc3516d4a79246e7cee52780ff0c6561c
Peter Easdown <info@pkclsoft.com>
2021-08-05 09:01:42 +1000

    Added NSTextAlignment and NSMutableParagraphStyle

--- Sources/UXKit/Common/UXTextView.swift
+++ Sources/UXKit/Common/UXTextView.swift
@@ -10,16 +10,24 @@
   import class UIKit.NSTextStorage
   import class UIKit.NSLayoutManager
   import class UIKit.NSTextContainer
+  import enum UIKit.NSTextAlignment
+  import class UIKit.NSMutableParagraphStyle
 
   public typealias NSTextStorage   = UIKit.NSTextStorage
   public typealias NSLayoutManager = UIKit.NSLayoutManager
   public typealias NSTextContainer = UIKit.NSTextContainer
+  public typealias NSTextAlignment = UIKit.NSTextAlignment
+  public typealias NSMutableParagraphStyle = UIKit.NSMutableParagraphStyle
 #else // macOS
   import class AppKit.NSTextStorage
   import class AppKit.NSLayoutManager
   import class AppKit.NSTextContainer
+  import enum AppKit.NSTextAlignment
+  import class AppKit.NSMutableParagraphStyle
 
   public typealias NSTextStorage   = AppKit.NSTextStorage
   public typealias NSLayoutManager = AppKit.NSLayoutManager
   public typealias NSTextContainer = AppKit.NSTextContainer
+  public typealias NSTextAlignment = AppKit.NSTextAlignment
+  public typealias NSMutableParagraphStyle = AppKit.NSMutableParagraphStyle
 #endif // macOS
