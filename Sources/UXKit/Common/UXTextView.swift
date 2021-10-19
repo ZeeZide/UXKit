35aceb89edd8f6fd61f373c46437ff126367be05
Helge Heß <devteam@zeezide.de>
2021-10-19 17:35:45 +0200

    Expose NSTextStorageEditActions/NSTextStorage.EditActions
    iOS warns on the former, macOS doesn't have the latter.
    Great times.

--- Sources/UXKit/Common/UXTextView.swift
+++ Sources/UXKit/Common/UXTextView.swift
@@ -3,23 +3,32 @@
 //  UXKit
 //
 //  Created by Helge Heß on 16.05.20.
-//  Copyright © 2020 ZeeZide GmbH. All rights reserved.
+//  Copyright © 2020-2021 ZeeZide GmbH. All rights reserved.
 //
 
 #if !os(macOS)
-  import class UIKit.NSTextStorage
-  import class UIKit.NSLayoutManager
-  import class UIKit.NSTextContainer
+  import class  UIKit.NSTextStorage
+  import class  UIKit.NSLayoutManager
+  import class  UIKit.NSTextContainer
+  import struct UIKit.NSTextStorageEditActions
 
   public typealias NSTextStorage   = UIKit.NSTextStorage
   public typealias NSLayoutManager = UIKit.NSLayoutManager
   public typealias NSTextContainer = UIKit.NSTextContainer
+
+  public typealias NSTextStorageEditActions = UIKit.NSTextStorage.EditActions
 #else // macOS
-  import class AppKit.NSTextStorage
-  import class AppKit.NSLayoutManager
-  import class AppKit.NSTextContainer
+  import class  AppKit.NSTextStorage
+  import class  AppKit.NSLayoutManager
+  import class  AppKit.NSTextContainer
+  import struct AppKit.NSTextStorageEditActions
 
   public typealias NSTextStorage   = AppKit.NSTextStorage
   public typealias NSLayoutManager = AppKit.NSLayoutManager
   public typealias NSTextContainer = AppKit.NSTextContainer
+
+  public typealias NSTextStorageEditActions = AppKit.NSTextStorageEditActions
+  public extension NSTextStorage {
+    typealias EditActions = NSTextStorageEditActions
+  }
 #endif // macOS
